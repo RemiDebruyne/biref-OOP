2603164a9af131b79e1f0518e066ddfd01a4317c
Remi Debruyne <remidebruyne@hotmail.com>
2024-06-04 13:26:26 +0200

    feat: encapsulation

--- car.js
+++ car.js
@@ -1,19 +1,60 @@
 class Car {
+  #color;
+  #brand;
+  #model;
+  #year;
+
   constructor(brand, model, year, color) {
-    this.color = color;
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
+    this.#color = color;
+    this.#brand = brand;
+    this.#model = model;
+    this.#year = year;
+  }
+
+  getColor() {
+    return this.#color;
+  }
+
+  setColor(value) {
+    this.#color = value;
+  }
+
+  getBrand() {
+    return this.#brand;
+  }
+
+  setBrand(value) {
+    this.#brand = value;
+  }
+
+  getModel() {
+    return this.#model;
+  }
+
+  setModel(value) {
+    this.#model = value;
+  }
+
+  getYear() {
+    return this.#year;
+  }
+
+  setYear(value) {
+    this.#year = value;
   }
 
   showDetails() {
-    console.log(this.brand);
-    console.log(this.model);
-    console.log(this.year);
-    console.log(this.color);
+    console.log(this.#brand);
+    console.log(this.#model);
+    console.log(this.#year);
+    console.log(this.#color);
   }
 }
 
 let myCar = new Car("Toyota", "Yaris", "2005", "Rouge");
+
+myCar.showDetails();
+
+myCar.setColor("jaune");
 
 myCar.showDetails();
